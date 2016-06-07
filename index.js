bb8236fa221a433f549d278cbba4f2c5b33e764d
arunasank <aruna@mapbox.com>
2016-06-06 22:14:50 -0400

    prototype.callApi

--- index.js
+++ index.js
@@ -11,20 +11,43 @@ var pagerduty = function(subdomain, token) {
 
 module.exports = pagerduty;
 
+pagerduty.prototype.callApi = function (url, qs, cb) {
+    var returnObject = {};
+    var params = {
+        url: url,
+        json: true,
+        headers: {
+            'Authorization': 'Token token=' + this.token
+        },
+        qs: qs
+    };
+    console.log(params);
+    request(params, function (err, res, data) {
+        if (err) {
+            return cb(err);
+        } else if (res.statusCode !== 200) {
+            return cb(new Error('Bad status code: ' + res.statusCode));
+        } else {
+            cb(null, data);
+        }
+    });
+};
+
 pagerduty.prototype.stream = function(options, services, interval) {
     var that = this;
     var stream = new Readable({objectMode: true});
     var seen = {};
     var calling = false;
 
-    stream._read = function() {
+    stream._read = function () {
         if (calling) return;
         query();
     };
 
-    var query = function() {
+    var query = function () {
         calling = true;
-        that.getIncidents(options, services, function(err, incidents) {
+        that.getIncidents(options, services, function(err, data) {
+            var incidents = data.incidents;
             if (err) return stream.emit('error', err);
             for (var i = 0; i < incidents.length; i++) {
                 var incident = incidents[i];
@@ -42,30 +65,19 @@ pagerduty.prototype.stream = function(options, services, interval) {
 };
 
 pagerduty.prototype.getIncidentNotes = function (ID, cb) {
-    var params = {
-        url: this.url + 'incidents/' + ID + '/notes',
-        json: true,
-        headers: {
-            'Authorization': 'Token token=' + this.token
-        }
-    };
-    request(params, function (err, res, data) {
-        if (err) return cb(err);
-        if (res.statusCode !== 200) {
-            return cb(new Error('Bad status code: ' + res.statusCode));
-        }
-        cb(null, data);
-    });
+    var url = this.url + 'incidents/' + ID + '/notes';
+    this.callApi(url, '');
 };
 
-pagerduty.prototype.getIncidents = function(options, services, cb) {
-    var that = this;
+pagerduty.prototype.getIncidents = function (options, services, cb) {
+    console.log('#### getIncidents options ', options, ' services ', services);
+    var url = this.url + 'incidents/';
     var qs = {
         'status': 'acknowledged,triggered',
         'sort_by': 'created_on:desc',
         'limit': 3
     };
-    _.keys(options).forEach(function(key) {
+    _.keys(options).forEach(function (key) {
         switch (key.toString) {
         case 'status':
             if (typeof options[key] != 'string') {
@@ -73,9 +85,9 @@ pagerduty.prototype.getIncidents = function(options, services, cb) {
             }
             qs.status = options[key];
             break;
-        case 'incident_key':
-            qs.incident_key = options[key];
-            break;
+        // case 'incident_key':
+        //     qs.incident_key = options[key];
+        //     break;
         case 'sort_by':
             qs.sort_by = options[key];
             break;
@@ -84,73 +96,39 @@ pagerduty.prototype.getIncidents = function(options, services, cb) {
 
     if (services.names) {
     // Resolve service names to ids
-        this.getServiceIds(services.names, function(err, ids) {
+        this.getServices(services.names, function (err, data) {
+
+            var services = data.services;
+
+            if (!services.length) {
+                return cb(new Error('No services found.'));
+            }
+
+            var ids = _(services).reduce(function (memo, service) {
+                if (_(services.names).indexOf(service.name) !== -1) {
+                    memo.push(service.id);
+                }
+                return memo;
+            }, []);
+
+            if (!ids.length) {
+                return cb(new Error('No matching services found.'));
+            }
 
             if (err) return cb(err);
-
             qs.service = (typeof ids != 'string') ? ids.toString() : ids;
-            var params = {
-                url: that.url + 'incidents',
-                json: true,
-                headers: {
-                    'Authorization': 'Token token=' + that.token
-                },
-                qs: qs
-            };
-            request(params, function (err, res, data) {
-                if (err) return cb(err);
-                if (res.statusCode != 200) {
-                    return cb(new Error('Bad status code: ' + res.statusCode));
-                }
-                cb(null, data.incidents);
-            });
+
+            this.callApi(url, qs, cb);
         });
     } else {
         qs.service = (typeof services.ids != 'string') ? services.ids.toString() : services.ids;
-        var params = {
-            url: this.url + 'incidents',
-            json: true,
-            headers: {
-                'Authorization': 'Token token=' + this.token
-            },
-            qs: qs
-        };
-        request(params, function (err, res, data) {
-            if (err) return cb(err);
-            if (res.statusCode != 200) {
-                return cb(new Error('Bad status code: ' + res.statusCode));
-            }
-            cb(null, data.incidents);
-        });
-
+        this.callApi(url, qs, cb);
     }
 };
 
-pagerduty.prototype.getServiceIds = function(names, cb) {
-    if ('string' == typeof names) names = names.split(',');
-    var params = {
-        url: this.url + 'services',
-        json: true,
-        headers: {
-            'Authorization': 'Token token=' + this.token
-        },
-        qs: {
-            //Currently we have 52 incidents in all, but the PagerDuty API limits by default to 25.
-            limit: 100
-        }
-    };
-    request(params, function (err, res, data) {
-        if (err) return cb(err);
-        if (res.statusCode != 200) {
-            return cb(new Error('Bad status code: ' + res.statusCode));
-        }
-        var services = data.services;
-        if (!services.length) return cb(new Error('No services found.'));
-        var ids = _(services).reduce(function(memo, service) {
-            if (_(names).indexOf(service.name) !== -1) memo.push(service.id);
-            return memo;
-        }, []);
-        if (!ids.length) return cb(new Error('No matching services found.'));
-        cb(null, ids);
-    });
+pagerduty.prototype.getServices = function (names, cb) {
+    if (typeof names === 'string') names = names.split(',');
+    var url = this.url + 'services';
+    var qs = {'limit': 100};
+    this.callApi(url, qs, cb);
 };
